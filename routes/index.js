b1497d728088910e2ab0853cebed910bc8f18076
Luke <LTaylor255@gmail.com>
2017-04-16 11:29:05 -0500

    Final updates

--- routes/index.js
+++ routes/index.js
@@ -7,26 +7,26 @@ var arr = {};
 var index = 0;
 
 router.get('/', (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs");
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body.passwd)
+    //console.log(req.body.passwd)
     cp.spawn("echo > /tmp/myfifo", [req.body.passwd], {"shell":true})
     var s = fs.createReadStream("/tmp/myfifo2");
     s.on("data", function(data) {
         arr[req.headers.host] = data;
-        console.log("arr=" + JSON.stringify(arr));
+        //console.log("arr=" + JSON.stringify(arr));
+        return res.redirect('/results');
     });
-    res.redirect('/')
 })
 
 router.get('/results', (req, res) => {
     console.log("arr = " + arr[req.headers.host]);
     //console.log("headers = " + JSON.stringify(req.headers));
-    res.render("results.ejs", {result: arr[req.headers.host]}, function(err, html) {
+    return res.render("results.ejs", {result: arr[req.headers.host]});/*, function(err, html) {
         res.send(html);
-    });
+    });*/
 })
 
 module.exports = router;
