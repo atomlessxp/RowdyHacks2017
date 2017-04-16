89776c5fbae08e0a80a521348ecc764840354d8c
Taylor <tcbrauer@gmail.com>
2017-04-16 09:35:58 -0500

    Add favicon module to server.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
+const favicon = require('serve-favicon')
+const path = require('path')
 const app = express()
 
 /**
@@ -16,6 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use('/', indexRoutes);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.listen(3000, function() {
     var s2 = fs.createWriteStream("/tmp/myfifo2");
